3b4d3aefdccb5d552fc7259ec6c9fee062b095ad
Shivam Dhir <shivam.dhir93@gmail.com>
2020-04-18 13:42:18 -0700

    Two Sum

--- Problem#1.cpp
+++ Problem#1.cpp
@@ -1,4 +1,9 @@
 /*
+Problem: https://leetcode.com/problems/two-sum/
+
+Solution: Make an unordered map with key as element and value as index
+of that element and check whether we find (target  - element) in the map
+
 Time complexity : O(n)
 Space complexity : O(n)
 n -> size of input vector
@@ -14,8 +19,6 @@ public:
         // second element stores index
         unordered_map<int, int> umap;
 
-        // Go over all elements and save the index with
-        // element as key
         // If (target - element) is already in the map
         // return the index of that element and current index
         for(int i = 0; i < nums.size(); i++)
@@ -25,9 +28,10 @@ public:
                 return {umap[target - nums[i]], i};
             }
 
+            // save the index as value with element as key
             umap[nums[i]] = i;
         }
+
         return {-1};
     }
-
 };
